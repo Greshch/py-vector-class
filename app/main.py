c25e48a03a985099d595cf7d4e6e457f9b0a42b4
Greshch <greshchook@gmail.com>
2024-12-12 22:54:18 +0200

    fix flake8

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, ordinate: float, abscissa: float) -> None:
+        self.x = round(ordinate, 2)
+        self.y = round(abscissa, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
 
-    def  __sub__(self, other: Vector) -> Vector | float:
+    def __sub__(self, other: Vector) -> Vector | float:
         return self.__add__(Vector(-other.x, -other.y))
 
     def __mul__(self, other: Vector | float) -> Vector | float:
@@ -24,9 +24,9 @@ class Vector:
             cls,
             start_point: tuple[float | int],
             end_point: tuple[float | int]
-    ):
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
-
+    ) -> Vector:
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - start_point[1])
 
     def get_length(self) -> float:
         return math.sqrt(pow(self.x, 2) + pow(self.y, 2))
@@ -36,7 +36,10 @@ class Vector:
         return Vector(self.x * inv_length, self.y * inv_length)
 
     def angle_between(self, other: Vector) -> int:
-        angle = math.acos(self * other / (self.get_length() * other.get_length()))
+        angle = math.acos(
+            self * other
+            / (self.get_length() * other.get_length())
+        )
         return round(math.degrees(angle))
 
     def get_angle(self) -> int:
@@ -47,6 +50,3 @@ class Vector:
         rotate_dx = self.x * math.cos(angle) - self.y * math.sin(angle)
         rotate_dy = self.x * math.sin(angle) + self.y * math.cos(angle)
         return Vector(rotate_dx, rotate_dy)
-
-
-
